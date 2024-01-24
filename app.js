980b7a4aee76b0ff9db692afc764e885e238f430
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-01-24 14:50:16 -0800

    Started JS

---
+++ app.js
@@ -0,0 +1,11 @@
+let calculator = document.getElementById("calculator");
+
+calculator.addEventListener("submit", calculate());
+
+function calculate(){
+    let equation = document.getElementById("inputEquation");
+    if (equation.value === "")
+    {
+        
+    }
+}
