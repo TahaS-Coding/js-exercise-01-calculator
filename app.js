5a162f5176101cbb81b8cc651f6eb0e9cc330f13
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-01-24 15:48:11 -0800

    Continued JS

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let calculator = document.getElementById("calculator");
 
-calculator.addEventListener("submit", calculate());
-
-function calculate(){
+function calculate() {
     let equation = document.getElementById("inputEquation");
-    if (equation.value === "")
-    {
-        
-    }
+    let calcToComplete = equation.value;
+    let completedCalc = eval(calcToComplete);
+    console.log(completedCalc);
 }
+
+if (calculator)
+{
+    console.log('real');
+}
+else {
+    console.log("null");
+}
+
