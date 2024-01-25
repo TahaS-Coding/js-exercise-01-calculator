2b744795f9416ed98a6a09306eee215fc7ea5202
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-01-25 15:50:00 -0800

    Started a new way of making the calculator

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ function calculate(event) {
     event.preventDefault();
     let equation = document.getElementById("inputEquation");
     let calcToComplete = equation.value;
+    calcToComplete.split("+","-","*","/");
+
+    for (let i = 0; i <= calcToComplete.length; i++){
+        if (calcToComplete[i+1] === "/" || calcToComplete[i+1] === "*")
+        {
+            
+        }
+    }
+
+
+
     let completedCalc = eval(calcToComplete);
     let answer = document.getElementById("answer");
     answer.value = completedCalc;
